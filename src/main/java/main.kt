423761bd5815fb7be83092a9c32c202a854eedff
Francisco Filho <francisco@capimgrosso.com>
2018-11-06 22:37:27 -0200

    add schema.sql

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -1,20 +1,140 @@
 package main
 
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import kotlin.concurrent.thread
+import java.math.BigDecimal
 
 //https://broker.negociecoins.com.br/api/v3/{PAR}/ticker
 //https://api.blinktrade.com/api/v1/BRL/trades
+//ticker -> vol
+
+data class Field(val name: String, val datatype: String, val type: String)
+data class Corretora(val name: String, val url: String, val fields: List<Field>, var taxas: Taxas?)
+data class Transacao(val crypto: BigDecimal, val real: BigDecimal)
+//com base na negocie coins
+data class Taxas (
+    val deposito: BigDecimal= BigDecimal("0.0"),
+    val depositoCrypto: BigDecimal = BigDecimal("0.0"),
+    val venda: BigDecimal   = BigDecimal("0.4"),
+    val compra: BigDecimal  = BigDecimal("0.3"),
+    val saque: BigDecimal   = BigDecimal("0.9"),
+    val saqueExtra: BigDecimal = BigDecimal("8.9"),
+    val limiteMinDiario: BigDecimal = BigDecimal("0.0001"),
+    val limiteMaxDiario: BigDecimal = BigDecimal("0.5"),
+    val saqueExtraTipo: String = "BRL",
+    val vendaTipo: String = "%",
+    val compraTipo: String = "%",
+    val bancos: List<String> = listOf("CAIXA", "BB")
+)
+
+// operacao,
+
+// corretora, operacao=DEPOSITO_DINHEIRO, moeda=BRL, unidade=R$, valor=0.0
+// corretora, operacao=SAQUE, moeda=BRL, unidade=percentual, valor=0.9
+// corretora, operacao=SAQUE, moeda=BRL, unidade=R$, valor=8.9
+// corretora, operacao=COMPRA, moeda=BRL, unidade=R$, valor=0.3
+// corretora, operacao=SALDO, moeda=BTC, valor=100%
+// corretora, operacao=SALDO, moeda=BRL, valor=100%
+
+// fluxo da operacao
+
+// deposito
+// corretora, operacao=DEPOSITO_DINHEIRO, BRL, sinal=-
+// corretora, operacao=SALDO, BRL, sinal=+
+// compra
+// corretora, operacao=DEPOSITO_DINHEIRO, BRL, sinal=-
+// corretora, operacao=COMPRA, BRL, sinal=-
+// corretora, operacao=SALDO, BTC, sinal=+
+// venda
+// corretora, operacao=VENDA, BTC, sinal=-
+// corretora, operacao=VENDA, BRL, sinal=-
+// corretora, operacao=SALDO, BTC, sinal=-
+// corretora, operacao=SALDO, BRL, sinal=+
+
+fun geto(o: JSONObject, field: Field): Any {
+    val keys = field.name.split("->")
+    val size = keys.size
+    if (size == 1) return getByType(o, keys[0], field.datatype)
+
+    var k1 = o.getJSONObject(keys[0])
+
+    for ((i, v) in keys.iterator().withIndex()) {
+        if (i == 0) continue
+        if (i == size -1)
+            return getByType(k1, v, field.datatype)
+    }
+
+    return k1
+}
+
+fun getByType(o: JSONObject, k: String, type: String): Any {
+    return when (type) {
+        "bigDecimal" -> o.getBigDecimal(k)
+        "double"    -> o.getDouble(k)
+        "text"      -> o.getString(k)
+        "integer"   -> o.getInt(k)
+        else        -> {
+            o.getJSONObject(k)
+        }
+    }
+}
 
 fun main(args: Array<String>){
-    thread() {
+    val f1 = listOf(
+        Field("vol", "bigDecimal", "Volume"),
+        Field("buy", "bigDecimal", "Comprar"),
+        Field("sell", "bigDecimal", "Vender")
+    )
+    val f2 = listOf(
+        Field("ticker->vol", "bigDecimal", "Volume"),
+        Field("ticker->buy", "bigDecimal", "Comprar"),
+        Field("ticker->sell", "bigDecimal", "Vender")
+    )
+    val f3 = listOf(
+        Field("baseVolume24", "bigDecimal", "Volume"),
+        Field("highestBid", "bigDecimal", "Comprar"),
+        Field("lowestAsk", "bigDecimal", "Vender")
+    )
+
+    val list = listOf(
+        Corretora("NegocieCoins", "https://broker.negociecoins.com.br/api/v3/BTC/ticker", f1, null),
+        Corretora("MercadoBitcoin", "https://www.mercadobitcoin.net/api/BTC/ticker/", f2, null),
+        Corretora("Foxbit","https://api.blinktrade.com/api/v1/BRL/ticker", f1, null),
+        Corretora("Braziliex","https://braziliex.com/api/v1/public/ticker/btc_brl", f3, null),
+        Corretora("BitcoinToYou", "https://www.bitcointoyou.com/api/ticker.aspx", f2, null)
+    )
+
+    for (l in list) {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url(l.url)
+            .get()
+            .build();
+        val response: Response = client.newCall(request).execute()
+        val jsonString = response.body()?.string()
+
+        val o = JSONObject(jsonString)
+
+        println("===============${l.name}==================")
+        for (f in l.fields){
+            val value = geto(o, f)
+            println("${f.name} \t= ${value}")
+        }
+    }
+
+    //val body = RequestBody.create()
+
+    /*thread() {
         cotarMercadoBitCoin()
     }
 
     thread(){
         cotarFoxbit()
-    }
+    }*/
 }
 
 fun cotarFoxbit(){
---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,98 @@
+﻿/* DROP SCHEMA arbitragem CASCADE; */
+
+CREATE SCHEMA arbitragem;
+
+CREATE TABLE arbitragem.corretoras (
+	id serial,
+	nome text NOT NULL,
+	url text NOT NULL,
+	criado_em timestamp NOT NULL DEFAULT now(),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE arbitragem.bancos (
+	id serial,
+	numero_banco int,
+	corretora_id int REFERENCES arbitragem.corretoras,
+	nome text,
+	agencia int,
+	agencia_digito smallint,
+	conta int,
+	conta_digito smallint,
+	criado_em timestamp NOT NULL DEFAULT now(),
+	PRIMARY KEY (id)
+);
+ 
+
+CREATE TABLE arbitragem.api (
+	id serial,
+	corretora_id int REFERENCES arbitragem.corretoras,
+	descricao text,
+	url text NOT NULL,
+	tipo text NOT NULL, -- BTC, BRL
+	criado_em timestamp NOT NULL DEFAULT now(),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE arbitragem.api_campos (
+	api_id int REFERENCES arbitragem.api,
+	campo text,
+	tipo_dado text NOT NULL,
+	transacao text NOT NULL,
+	criado_em timestamp NOT NULL DEFAULT now(),
+	PRIMARY KEY(api_id, campo)
+);
+
+CREATE TABLE arbitragem.taxas (
+	id serial,
+	corretora_id int REFERENCES arbitragem.corretoras,
+	taxa text NOT NULL,
+	metrica text NOT NULL,
+	valor numeric(14, 9) NOT NULL,
+	ordem smallint NOT NULL DEFAULT 0,
+	adicionado_em timestamp NOT NULL DEFAULT now(),
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE arbitragem.cotacoes (
+	id serial,
+	api_id int REFERENCES arbitragem.api,
+	valor numeric(14, 9) NOT NULL,
+	cotado_em timestamp NOT NULL DEFAULT now(),
+	PRIMARY KEY (id)
+);
+
+INSERT INTO arbitragem.corretoras VALUES (1, 'NegocieCoins', 'https://broker.negociecoins.com.br');
+INSERT INTO arbitragem.corretoras VALUES (2, 'MercadoBitcoin', 'https://www.mercadobitcoin.net');
+INSERT INTO arbitragem.corretoras VALUES (3, 'Foxbit', 'https://api.blinktrade.com');
+INSERT INTO arbitragem.corretoras VALUES (4, 'Braziliex', 'https://braziliex.com');
+INSERT INTO arbitragem.corretoras VALUES (5, 'BitcoinToYou', 'https://www.bitcointoyou.com');
+
+INSERT INTO arbitragem.api VALUES (1, 1, 'Ticker', 'https://broker.negociecoins.com.br/api/v3/BTC/ticker', 'BTC');
+INSERT INTO arbitragem.api VALUES (2, 2, 'Ticker', 'https://www.mercadobitcoin.net/api/BTC/ticker/', 'BTC');
+INSERT INTO arbitragem.api VALUES (3, 3, 'Ticker', 'https://api.blinktrade.com/api/v1/BRL/ticker', 'BTC');
+INSERT INTO arbitragem.api VALUES (4, 4, 'Ticker', 'https://braziliex.com/api/v1/public/ticker/btc_brl', 'BTC');
+INSERT INTO arbitragem.api VALUES (5, 5, 'Ticker', 'https://www.bitcointoyou.com/api/ticker.aspx', 'BTC');
+
+INSERT INTO arbitragem.api_campos VALUES (1, 'vol', 'bigInteger', 'Volume');
+INSERT INTO arbitragem.api_campos VALUES (1, 'buy', 'bigInteger', 'Comprar');
+INSERT INTO arbitragem.api_campos VALUES (1, 'sell', 'bigInteger', 'Vender');
+
+INSERT INTO arbitragem.api_campos VALUES (2, 'ticker->vol', 'bigInteger', 'Volume');
+INSERT INTO arbitragem.api_campos VALUES (2, 'ticker->buy', 'bigInteger', 'Comprar');
+INSERT INTO arbitragem.api_campos VALUES (2, 'ticker->sell', 'bigInteger', 'Vender');
+
+INSERT INTO arbitragem.api_campos VALUES (3, 'vol', 'bigInteger', 'Volume');
+INSERT INTO arbitragem.api_campos VALUES (3, 'buy', 'bigInteger', 'Comprar');
+INSERT INTO arbitragem.api_campos VALUES (3, 'sell', 'bigInteger', 'Vender');
+
+INSERT INTO arbitragem.api_campos VALUES (4, 'baseVolume24', 'bigInteger', 'Volume');
+INSERT INTO arbitragem.api_campos VALUES (4, 'highestBid', 'bigInteger', 'Comprar');
+INSERT INTO arbitragem.api_campos VALUES (4, 'lowestAsk', 'bigInteger', 'Vender');
+
+INSERT INTO arbitragem.api_campos VALUES (5, 'ticker->vol', 'bigInteger', 'Volume');
+INSERT INTO arbitragem.api_campos VALUES (5, 'ticker->buy', 'bigInteger', 'Comprar');
+INSERT INTO arbitragem.api_campos VALUES (5, 'ticker->sell', 'bigInteger', 'Vender');
+
+SELECT setval('arbitragem.corretoras_id_seq', 	(SELECT COALESCE(MAX(id), 1) FROM arbitragem.corretoras));
+SELECT setval('arbitragem.api_id_seq', 			(SELECT COALESCE(MAX(id), 1) FROM arbitragem.api));
